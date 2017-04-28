59b05aac7480d4c1707279f0b4575726421fef06
YiniZhang <yz3005@columbia.edu>
2017-04-27 20:36:15 -0500

    update cluster functions

--- lib/cluster.R
+++ lib/cluster.R
@@ -3,12 +3,20 @@ cnt <- odbcConnect(dsn = "project5243", uid = "bpeng", pwd = "qqqq123456")
 cat_sample <- RODBC::sqlQuery(cnt, query = "SELECT * FROM Project5261.dbo.cat_sample")
 topic_sample <- RODBC::sqlQuery(cnt, query = "SELECT * FROM Project5261.dbo.topic_sample")
 
+cat_id_all <- sqlQuery(cnt, query = "select distinct cat_id from Project5261.dbo.doc_category")
+topic_id_all <- sqlQuery(cnt, query = "select distinct topic_id from Project5261.dbo.documents_topics")
+
 source("./functions.R")
-cat <- cluster2(df_sample = cat_sample, variable = "cat_id",k=10)
-topic <- cluster3(df_sample = topic_sample, variable = "topic_id",k=10)
+cat <- cluster_cat(df_sample = cat_sample, id_all = cat_id_all, k=5)
+cat_data <- cat[[1]]
+cat_model <- cat[[2]]
+topic <- cluster_topic(df_sample = topic_sample, id_all = topic_id_all, k=10)
+topic_data <- topic[[1]]
+topic_model <- topic[[2]]
 
-write.csv(cat, file = "../output/cat_clustered.csv")
-write.csv(topic, file = "../output/topic_clustered.csv")
+write.csv(cat_data, file = "../output/cat_clustered.csv")
 
+write.csv(topic_data, file = "../output/topic_clustered.csv")
 
+save(cat_model, topic_model, file = "../output/clustermodel.RData")
 
--- lib/functions.R
+++ lib/functions.R
@@ -1,24 +1,24 @@
 # Function: Reorganize df with all categories/topics columns
 # Input  : df_sample: sample (dataframe); 
-#          df_all: all data (dataframe):
-#          variable: column name to cluster upon (string)
+#          id_all: all unique ids (either for categories or for topics)
 # Output : reorganized data frame (each column corresponds to one category/topic)
 
-reorganize <- function(df_sample, df_all, variable){
+reorganize <- function(df_sample, id_all){
   
   a <- colnames(df_sample)[-1]
-  b <- unique(df_all[,variable])
   
   # Find the column names that need to be added
   add_col <- c()
-  for (x in b){
-    if (sum(a==x)==0){
+  for (x in id_all){
+    if (sum(a==rep(x,length(a)))==0){
       add_col <- c(add_col, x)
     }
   }
+  
   add_df <- as.data.frame(matrix(0, 
                                  nrow = nrow(df_sample), 
                                  ncol = length(add_col)))
+  
   colnames(add_df) <- add_col
   df_sample_com <- cbind(df_sample, add_df)
   
@@ -33,23 +33,38 @@ reorganize <- function(df_sample, df_all, variable){
 
 
 # Function: Cluster documents based on category/topic
-# Input   : df_sample, df_all, variable (as in function "reorganize")
-#           key_name (cat_id or topic_id)
+# Input   : df_sample, id_all, k (centers for kmeans)
 # Output  : dataframe (columns: doc_id, new_cluster)
 
-cluster <- function(df_sample, df_all, variable){
+cluster_cat <- function(df_sample, id_all=cat_id_all, k){
   
+  # long to wide
   new_doc_sample <- tidyr::spread(df_sample, key = cat_id, value = con, fill = 0)
   
-  doc_organized <- reorganize(df_sample = new_doc_sample, 
-                              df_all = df_all,
-                              variable = variale)
+  doc_organized <- reorganize(df_sample = new_doc_sample,id_all)
+  
   # kmeans
-  doc_cluster <- kmeans(x=doc_organized[,-ncol(doc_organized)])
+  doc_cluster <- kmeans(x=doc_organized[,-ncol(doc_organized)], centers = k)
   doc_organized$new_cluster <- doc_cluster$cluster
   doc_final <- doc_organized[, c("doc_id", "new_cluster")]
   
-  return(doc_final)
+  return(list(doc_final, doc_cluster))
+}
+
+
+cluster_topic <- function(df_sample, id_all=topic_id_all, k){
+  
+  # long to wide
+  new_doc_sample <- tidyr::spread(df_sample, key = topic_id, value = con, fill = 0)
+  
+  doc_organized <- reorganize(df_sample = new_doc_sample, id_all)
+  
+  # kmeans
+  doc_cluster <- kmeans(x=doc_organized[,-ncol(doc_organized)], centers = k)
+  doc_organized$new_cluster <- doc_cluster$cluster
+  doc_final <- doc_organized[, c("doc_id", "new_cluster")]
+  
+  return(list(doc_final, doc_cluster))
 }
 
 
