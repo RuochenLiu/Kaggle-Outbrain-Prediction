ad2376dd68de5e33ff7493aaf093e065be5095af
bpeng93 <lloyd_1993@126.com>
2017-04-27 22:15:22 -0400

    update

--- lib/preprocess.R
+++ lib/preprocess.R
@@ -2,7 +2,7 @@ library(RODBC)
 cnt <- odbcConnect(dsn = "project5243", uid = "bpeng", pwd = "qqqq123456")
 final <- RODBC::sqlQuery(cnt, query = "SELECT * FROM Project5261.dbo.final")
 full_index <- unique(final$display_id)
-cut_index <- full_index[1:200000]
+cut_index <- sample(full_index, 200000)
 cut <- final[which(final$display_id %in% cut_index), ]
 
 cut <- cut[,-c(2,3,4,7)]
@@ -12,4 +12,4 @@ for (i in 1:ncol(cut)) {
   cut[,i] <- as.factor(cut[,i])
 }
 
-save(cut, file = "../data/final_cutted.rdata")
+save(cut, file = "../data/final_uncutted.rdata")
